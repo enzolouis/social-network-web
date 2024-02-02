65b9b42c0b423006fa8bb61609b892e333043044
Ember <enzo.louis@etu.iut-tlse3.fr>
2024-02-03 00:01:48 +0100

    Refactor header to use easily in every page

---
+++ js/header.js
@@ -0,0 +1,12 @@
+function increaseHeaderWidth() {
+	document.getElementById("header").style.width = '300px'
+	Array.from(document.getElementsByClassName("precision")).forEach((span) => {
+		span.style.opacity = "1";
+	})
+}
+function decreaseHeaderWidth() {
+	document.getElementById("header").style.width = '90px'
+	Array.from(document.getElementsByClassName("precision")).forEach((span) => {
+		span.style.opacity = "0";
+	})
+}
