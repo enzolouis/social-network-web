9907b6e47c690757f1c016ecd4e825cf4d8365ed
Maxence <133963749+MaxenceMB@users.noreply.github.com>
2024-02-02 18:59:44 +0100

    Create chat page layout

---
+++ php/chat.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel = "stylesheet" href = "../css/chat.css">
+    </head>    
+
+    <body>
+
+        <div id = "discussions">
+        </div>
+
+        <!-- W in the shaaat -->
+        <div id = "chat">
+
+            <!-- Chat header  -->
+            <div id = "chat-header">
+                <img id = "chat-header-pp" src = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">
+                <div id = "chat-header-infos">
+                    <h1>Ibrahim le bon immigré</h1>
+                    <p>Un sous-titre sympa</p>
+                </div>
+                
+            </div>
+
+            <!-- Chat box -->
+            <div id = "chat-box">
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">Ouais ouais ouais</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">ouais</p>
+                </div>
+                <div class = "msg" id = "user_me">
+                    <p class = "msg-text">Ouais ouais ouais !!!!!</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">euh ouais</p>
+                </div>
+                <div class = "msg" id = "user_me">
+                    <p class = "msg-text">Ouais ?</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">Ouais...</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">Ouais ouais ouais</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">ouais</p>
+                </div>
+                <div class = "msg" id = "user_me">
+                    <p class = "msg-text">Ouais ouais ouais !!!!!</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">euh ouais</p>
+                </div>
+                <div class = "msg" id = "user_me">
+                    <p class = "msg-text">Ouais ?</p>
+                </div>
+                <div class = "msg" id = "user_other">
+                    <p class = "msg-text">Ouais...</p>
+                </div>
+            </div>
+
+            <!-- Message input -->
+            <div id = "chat-message">
+                <form action = "#">
+                    <input type = "text"   id = "chat-message-text"   name = "text">
+                    <input type = "submit" id = "chat-message-submit" name = "submit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html> 
