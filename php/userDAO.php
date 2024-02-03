b96c156f3d5fdbf3b6c3d59bfca6500d6457f87c
Will <cuteamatic@gmail.com>
2024-02-03 12:38:28 +0100

    Update userDAO.php
    Addition of a password checking function for the login and some doc.

--- php/userDAO.php
+++ php/userDAO.php
@@ -35,14 +35,39 @@
         }
 
     }
-
+    
+    /**
+     * Checks if the password entered by the user matches the hashed password stored in the database for the specified login.
+     *
+     * @param  PDO $pdo
+     * @param  mixed $login
+     * @param  mixed $password
+     * @return bool
+     */
+    function isPassValid(PDO $pdo, $login, $password): bool {
+        $user = getUserById($pdo, $login);
+    
+        if ($user && (password_verify($password, $user->getPassword()))) {
+            return true;
+        }
+    
+        return false;
+    }
+    
+    /**
+     * Checks if a user with the login $id exists in the database.
+     *
+     * @param  PDO $pdo
+     * @param  mixed $id
+     * @return User
+     */
     function getUserById(PDO $pdo, string $id) : User | null {
         $stmt = prepare($pdo, "SELECT * FROM user WHERE login = ?");
         execute($stmt, [$id]);
+
         if ($user = $stmt->fetch()) {
             return new User($user["login"], $user["username"], $user["password"], $user["description"]);
         }
+
         return null;
     }
-    
-?>
