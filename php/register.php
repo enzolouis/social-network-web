764a71ed81b0fe060ec10b2d2ce2d33af2a55ee1
Will <cuteamatic@gmail.com>
2024-02-03 15:54:58 +0100

    Backend registration

---
+++ php/register.php
@@ -0,0 +1,32 @@
+<?php
+
+require("functions.php");
+require("userDAO.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $login = $_POST["login"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $passwordverify = $_POST["password-verify"];
+
+    $pdo = createConnection();
+
+    $user = getUserById($pdo, $login);
+
+    if (!$user) {
+        if ($password !== $passwordverify) {
+            header("Location: ../html/register.html");
+            exit();
+        }
+
+        if (strlen($password) < 8 || !preg_match('/[!@#$%^&*(),.?":{}|<>]/', $password)) {
+            header("Location: ../html/register.html");
+            exit();
+        }
+
+        ajouter($pdo, $login, $username, $password);
+    }
+
+    header("Location: ../html/register.html");
+}
